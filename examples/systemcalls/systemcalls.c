56d5027642ed75184dc631494b76e8d2a5aa3e36
Michael Gunlock <1138684+gunlock@users.noreply.github.com>
2025-08-16 15:27:31 -0600

    Implemented do_system()

--- examples/systemcalls/systemcalls.c
+++ examples/systemcalls/systemcalls.c
@@ -1,4 +1,5 @@
 #include "systemcalls.h"
+#include <stdlib.h>
 
 /**
  * @param cmd the command to execute with system()
@@ -6,94 +7,102 @@
  *   successfully using the system() call, false if an error occurred,
  *   either in invocation of the system() call, or if a non-zero return
  *   value was returned by the command issued in @param cmd.
-*/
-bool do_system(const char *cmd)
-{
+ */
+bool do_system(const char *cmd) {
 
-/*
- * TODO  add your code here
- *  Call the system() function with the command set in the cmd
- *   and return a boolean true if the system() call completed with success
- *   or false() if it returned a failure
-*/
+  /*
+   * TODO  add your code here
+   *  Call the system() function with the command set in the cmd
+   *   and return a boolean true if the system() call completed with success
+   *   or false() if it returned a failure
+   */
+  if (!cmd || cmd[0] == '\0') {
+    return false;
+  }
 
-    return true;
+  int result = system(cmd);
+
+  // Based on my interpreation of the instructions, I am only evaluating the
+  // the system() result, not the child exit status.  Per the manual page
+  // for system(), return values of -1  (child could not be created) and
+  // 127 (shell could not be executed in the child process) indicates
+  // failure of the system(). However, they do not describe the state
+  // of the child exit.
+
+  return (result != -1 || result != 127);
 }
 
 /**
-* @param count -The numbers of variables passed to the function. The variables are command to execute.
-*   followed by arguments to pass to the command
-*   Since exec() does not perform path expansion, the command to execute needs
-*   to be an absolute path.
-* @param ... - A list of 1 or more arguments after the @param count argument.
-*   The first is always the full path to the command to execute with execv()
-*   The remaining arguments are a list of arguments to pass to the command in execv()
-* @return true if the command @param ... with arguments @param arguments were executed successfully
-*   using the execv() call, false if an error occurred, either in invocation of the
-*   fork, waitpid, or execv() command, or if a non-zero return value was returned
-*   by the command issued in @param arguments with the specified arguments.
-*/
+ * @param count -The numbers of variables passed to the function. The variables
+ * are command to execute. followed by arguments to pass to the command Since
+ * exec() does not perform path expansion, the command to execute needs to be an
+ * absolute path.
+ * @param ... - A list of 1 or more arguments after the @param count argument.
+ *   The first is always the full path to the command to execute with execv()
+ *   The remaining arguments are a list of arguments to pass to the command in
+ * execv()
+ * @return true if the command @param ... with arguments @param arguments were
+ * executed successfully using the execv() call, false if an error occurred,
+ * either in invocation of the fork, waitpid, or execv() command, or if a
+ * non-zero return value was returned by the command issued in @param arguments
+ * with the specified arguments.
+ */
 
-bool do_exec(int count, ...)
-{
-    va_list args;
-    va_start(args, count);
-    char * command[count+1];
-    int i;
-    for(i=0; i<count; i++)
-    {
-        command[i] = va_arg(args, char *);
-    }
-    command[count] = NULL;
-    // this line is to avoid a compile warning before your implementation is complete
-    // and may be removed
-    command[count] = command[count];
+bool do_exec(int count, ...) {
+  va_list args;
+  va_start(args, count);
+  char *command[count + 1];
+  int i;
+  for (i = 0; i < count; i++) {
+    command[i] = va_arg(args, char *);
+  }
+  command[count] = NULL;
+  // this line is to avoid a compile warning before your implementation is
+  // complete and may be removed
+  command[count] = command[count];
 
-/*
- * TODO:
- *   Execute a system command by calling fork, execv(),
- *   and wait instead of system (see LSP page 161).
- *   Use the command[0] as the full path to the command to execute
- *   (first argument to execv), and use the remaining arguments
- *   as second argument to the execv() command.
- *
-*/
+  /*
+   * TODO:
+   *   Execute a system command by calling fork, execv(),
+   *   and wait instead of system (see LSP page 161).
+   *   Use the command[0] as the full path to the command to execute
+   *   (first argument to execv), and use the remaining arguments
+   *   as second argument to the execv() command.
+   *
+   */
 
-    va_end(args);
+  va_end(args);
 
-    return true;
+  return true;
 }
 
 /**
-* @param outputfile - The full path to the file to write with command output.
-*   This file will be closed at completion of the function call.
-* All other parameters, see do_exec above
-*/
-bool do_exec_redirect(const char *outputfile, int count, ...)
-{
-    va_list args;
-    va_start(args, count);
-    char * command[count+1];
-    int i;
-    for(i=0; i<count; i++)
-    {
-        command[i] = va_arg(args, char *);
-    }
-    command[count] = NULL;
-    // this line is to avoid a compile warning before your implementation is complete
-    // and may be removed
-    command[count] = command[count];
+ * @param outputfile - The full path to the file to write with command output.
+ *   This file will be closed at completion of the function call.
+ * All other parameters, see do_exec above
+ */
+bool do_exec_redirect(const char *outputfile, int count, ...) {
+  va_list args;
+  va_start(args, count);
+  char *command[count + 1];
+  int i;
+  for (i = 0; i < count; i++) {
+    command[i] = va_arg(args, char *);
+  }
+  command[count] = NULL;
+  // this line is to avoid a compile warning before your implementation is
+  // complete and may be removed
+  command[count] = command[count];
 
+  /*
+   * TODO
+   *   Call execv, but first using https://stackoverflow.com/a/13784315/1446624
+   * as a refernce, redirect standard out to a file specified by outputfile. The
+   * rest of the behaviour is same as do_exec()
+   *
+   */
 
-/*
- * TODO
- *   Call execv, but first using https://stackoverflow.com/a/13784315/1446624 as a refernce,
- *   redirect standard out to a file specified by outputfile.
- *   The rest of the behaviour is same as do_exec()
- *
-*/
+  va_end(args);
 
-    va_end(args);
-
-    return true;
+  return true;
 }
