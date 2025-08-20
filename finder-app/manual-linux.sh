7090394fe601a7ca8a7e2cc0229ef9bae7d600fb
Michael Gunlock <1138684+gunlock@users.noreply.github.com>
2025-08-20 17:43:05 -0600

    Implemented assignment in manual-linux.sh

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -5,26 +5,40 @@
 set -e
 set -u
 
-OUTDIR=/tmp/aeld
+# 1.a assign $OUTDIR to $1 or default (/tmp/aeld) if not provided
+OUTDIR=${1:-/tmp/aeld} 
+
+# 1.b create $OUTDIR if it does not exist. If failure, exit.
+# NOTE: set -e (exit on any error) will exit if mkdir fails
+if [ ! -d "$OUTDIR" ]; then
+    mkdir -p "$OUTDIR"
+fi
+
 KERNEL_REPO=git://git.kernel.org/pub/scm/linux/kernel/git/stable/linux-stable.git
 KERNEL_VERSION=v5.15.163
 BUSYBOX_VERSION=1_33_1
 FINDER_APP_DIR=$(realpath $(dirname $0))
 ARCH=arm64
 CROSS_COMPILE=aarch64-none-linux-gnu-
+TOOLCHAIN_DIR=/home/michael/toolchains/arm-gnu-toolchain-13.3.rel1-x86_64-aarch64-none-linux-gnu
+TOOLCHAIN_SYSROOT=${TOOLCHAIN_DIR}/aarch64-none-linux-gnu/libc
+SCRIPT_DIR="$PWD"
 
 if [ $# -lt 1 ]
 then
 	echo "Using default directory ${OUTDIR} for output"
 else
-	OUTDIR=$1
+    # see above
+	#  OUTDIR=$1
 	echo "Using passed directory ${OUTDIR} for output"
 fi
 
-mkdir -p ${OUTDIR}
+# see above
+# mkdir -p ${OUTDIR}
 
 cd "$OUTDIR"
 if [ ! -d "${OUTDIR}/linux-stable" ]; then
+    # 
     #Clone only if the repository does not exist.
 	echo "CLONING GIT LINUX STABLE VERSION ${KERNEL_VERSION} IN ${OUTDIR}"
 	git clone ${KERNEL_REPO} --depth 1 --single-branch --branch ${KERNEL_VERSION}
@@ -34,7 +48,23 @@ if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
     echo "Checking out version ${KERNEL_VERSION}"
     git checkout ${KERNEL_VERSION}
 
-    # TODO: Add your kernel build steps here
+
+    # COMPLETED: 1.c - Add your kernel build steps here
+    
+    # Step 1: Deep clean kernel build tree removing .conig file with any existing configs
+    make ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu- mrproper
+
+    # Step 2: Configure for our "virt" arm dev board we will simulate in QEMU
+    make ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu- defconfig
+
+    # Step 3: Build kernal image for booting with QEMU
+    make -j4 ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu- all
+
+    # Step 4: Build kernel modules
+    make ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu- modules
+
+    # Step 5: Build device tree (dtbs == device tree)
+    make ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu- dtbs
 fi
 
 echo "Adding the Image in outdir"
@@ -44,37 +74,91 @@ cd "$OUTDIR"
 if [ -d "${OUTDIR}/rootfs" ]
 then
 	echo "Deleting rootfs directory at ${OUTDIR}/rootfs and starting over"
-    sudo rm  -rf ${OUTDIR}/rootfs
+    sudo rm -rf ${OUTDIR}/rootfs
 fi
 
-# TODO: Create necessary base directories
+# COMPLETED: 1.e Create necessary base directories
+mkdir -p ${OUTDIR}/rootfs
+pushd ${OUTDIR}/rootfs
+    echo "Creating rootfs directory tree here: ${OUTDIR}/rootfs"
+    mkdir -p bin dev etc home lib lib64 proc sbin sys tmp usr var 
+    mkdir -p usr/bin /usr/lib /usr/sbin
+    mkdir -p var/log
+    mkdir -p home/conf
+popd
+
+# Copy the the ${ARCH} kernel boot image, in this case ARM64.
+# This is the compressed bootable kernel image for the compilation
+# step above.
+if [ -f "${OUTDIR}/Image" ]; then
+    rm "${OUTDIR}/Image"
+fi
+
+# Copy 
+cp ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ${OUTDIR}
 
 cd "$OUTDIR"
-if [ ! -d "${OUTDIR}/busybox" ]
-then
-git clone git://busybox.net/busybox.git
+if [ ! -d "${OUTDIR}/busybox" ]; then
+git clone https://git.busybox.net/busybox
     cd busybox
     git checkout ${BUSYBOX_VERSION}
-    # TODO:  Configure busybox
+    # COMPLETED:  Configure busybox
+    make clean
+    make defconfig  # generate a default config file (.config)
 else
     cd busybox
 fi
 
-# TODO: Make and install busybox
+# COMPLETED: Make and install busybox
+make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE}
+make CONFIG_PREFIX="${OUTDIR}/rootfs" ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} install
 
 echo "Library dependencies"
-${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter"
-${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
+pushd ${OUTDIR}/rootfs
+    ${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter"
+    ${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
+popd
 
-# TODO: Add library dependencies to rootfs
+# COMPLETED: Add library dependencies to rootfs
 
-# TODO: Make device nodes
+# dependency for busybox program interpreter (i.e. /lib/ld-linux-aarch64.so.1) and shared libs
+BB_DEPS_PROG_INTERPRETER=$(${TOOLCHAIN_DIR}/bin/${CROSS_COMPILE}readelf -a ${OUTDIR}/rootfs/bin/busybox | \
+    grep "program interpreter" | sed 's/.*: \([^]]*\).*/\1/')
+BB_DEPS_SHARED_LIBS=$(${TOOLCHAIN_DIR}/bin/${CROSS_COMPILE}readelf -a ${OUTDIR}/rootfs/bin/busybox | \
+    grep "Shared library" | sed 's/.*\[\(.*\)\]/\1/')
 
-# TODO: Clean and build the writer utility
+# copy libs from toolchain sysroot to rootfs for target
+cp ${TOOLCHAIN_SYSROOT}/${BB_DEPS_PROG_INTERPRETER} ${OUTDIR}/rootfs/lib
+for lib in ${BB_DEPS_SHARED_LIBS}; do
+    cp ${TOOLCHAIN_SYSROOT}/lib64/$lib ${OUTDIR}/rootfs/lib64
+done
 
-# TODO: Copy the finder related scripts and executables to the /home directory
+# COMPLETED: Make device nodes
+
+# null device: Major 1, Minor 3
+# console device: Major 5, Minor 1
+pushd ${OUTDIR}/rootfs
+    sudo mknod -m 666 dev/null c 1 3
+    sudo mknod -m 666 dev/console c 5 1
+popd
+
+# COMPLETED: Clean and build the writer utility
+cd "$SCRIPT_DIR"
+make clean
+make CROSS_COMPILE=${CROSS_COMPILE}
+
+# COMPLETED: Copy the finder related scripts and executables to the /home directory
 # on the target rootfs
+cp *.sh "${OUTDIR}/rootfs/home"
+cp writer "${OUTDIR}/rootfs/home"
+cp conf/username.txt "${OUTDIR}/rootfs/home/conf"
+cp conf/assignment.txt "${OUTDIR}/rootfs/home/conf"
 
-# TODO: Chown the root directory
+# COMPLETED: Chown the root directory
+cd $OUTDIR/rootfs
+sudo chown -R root:root *
 
-# TODO: Create initramfs.cpio.gz
+# COMPLETED: Create initramfs.cpio.gz (ram disk for qemu)
+cd "$OUTDIR/rootfs"
+find . | cpio -H newc -ov --owner root:root > ${OUTDIR}/initramfs.cpio
+gzip -f "$OUTDIR/initramfs.cpio"
